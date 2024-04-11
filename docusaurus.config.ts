ce4b441877e41de3ff745d8bb6f88ab91e2f450c
MarkSchmidty <MarkSchmidty@users.noreply.github.com>
2024-04-11 17:36:42 -0400

    partial branding fix

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -59,7 +59,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'My Site',
+      title: 'Naptha AI',
       logo: {
         alt: 'My Site Logo',
         src: 'img/logo.svg',
--- src/components/HomepageFeatures/index.tsx
+++ src/components/HomepageFeatures/index.tsx
@@ -10,32 +10,33 @@ type FeatureItem = {
 
 const FeatureList: FeatureItem[] = [
   {
-    title: 'Easy to Use',
+    title: 'Simple and Scalable',
     Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Naptha makes coordinating decentralized AI services & workflows simple 
+        and scalable. We're enabling the next generation of apps and use cases 
+        on decentralized infrastructure.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Operator Nodes',
     Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Operator Nodes orchestrate the completion of tasks using AI workflow 
+        & agent services.
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Async by Design',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Enjoy an LLM Workflow Orchestration Engine that supports async 
+        messaging for running workflow steps on different nodes.
       </>
     ),
   },

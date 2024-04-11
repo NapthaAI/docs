bd6a354158f4543b9dd69e4d6b4bfd71650cff1d
MarkSchmidty <MarkSchmidty@users.noreply.github.com>
2024-04-11 17:00:39 -0400

    fix edit links

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,14 +39,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/NapthaAI/napthaai.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/NapthaAI/napthaai.github.io/tree/main/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/NapthaAI/napthaai.github.io/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/NapthaAI/napthaai.github.io/tree/main/',
         },
         theme: {
           customCss: './src/css/custom.css',

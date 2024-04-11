b84c1160d724c0f4892ddb690c1a0d15627713d5
MarkSchmidty <MarkSchmidty@users.noreply.github.com>
2024-04-11 16:56:07 -0400

    add back intro to sidebar

--- sidebars.ts
+++ sidebars.ts
@@ -3,6 +3,7 @@ import type {SidebarsConfig} from '@docusaurus/plugin-content-docs';
 const sidebars: SidebarsConfig = {
   docs: [
     {
+      'intro',
       type: 'category',
       label: 'Getting Started',
       items: [
